a549d9c0087c124dc1f19b390c1b68b0adae5d97
alexisBes <alexis.bessonnat@epitech.eu>
2023-07-08 01:10:22 +0200

    Ajout de la génération du terrain

---
+++ Assets/State.cs
@@ -0,0 +1,23 @@
+
+public class State
+{
+    // new tile
+    public static int SPAWN_WATER = 0x01;
+    public static int SPAWN_SAND= 0x02;
+    public static int SPAWN_LAND= 0x03;
+    public static int SPAWN_TOWN= 0x05;
+
+    // action
+    public static int SPAWN_TEMPEST  = 0x01;
+    
+    // MIX
+    public static int MIX_WITH_SAND  = 0x02;
+    public static int MIX_WITH_WATER  = 0x04;
+
+
+    private State()
+    { }
+    public State getInsance { get; private set; }
+    public static int state;
+
+}
