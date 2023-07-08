f8e4cd00227f669b0a22626a244c9207bf0eaa4f
alexisBes <alexis.bessonnat@epitech.eu>
2023-07-08 05:34:16 +0200

    Fix merge conflict

--- Assets/Terrain/script/our_terrain.cs
+++ Assets/Terrain/script/our_terrain.cs
@@ -10,10 +10,10 @@ public class Our_Terrain : MonoBehaviour
 {
     public GameObject tile_prefab;
 
-    public Material material;
 
     public int width = 8;
     public int height = 7;
+    const float TILE_STEP = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -26,35 +26,30 @@ public class Our_Terrain : MonoBehaviour
         if (centreW == width) centreW--;
         if (centreH == height) centreH--;
         Debug.Log("X : " + centreW + ", Y : " + centreH);
-        for (int i = 0; i < width; i++)
+        for (int y = 0; y < height; y++)
         {
-            for (int j = 0; j < height; j++)
+            for (int x = 0; x < width; x++)
             {
                 int newFlags = 0x00;
-                if ((i >= centreW-1 && i <= centreW+1) && (j <= centreH +1  && j >= centreH -1))
+                if ((y >= centreW - 1 && y <= centreW + 1) && (x <= centreH + 1 && x >= centreH - 1))
                     newFlags = Random.Range(1, 0x05);
-                if (i == centreW && j == centreH) { newFlags = State.SPAWN_TOWN; }
-                GameObject game = Instantiate(tile_prefab, position, tile_prefab.transform.rotation);
-                Tile terrain = game.GetComponent<Tile>();
+                if (y == centreW && x == centreH) { newFlags = State.SPAWN_TOWN; }
+                GameObject tile = Instantiate(tile_prefab, position, tile_prefab.transform.rotation);
+                Tile terrain = tile.GetComponent<Tile>();
                 terrain.flags = newFlags;
 
-                PlayerInput pi = game.GetComponent<PlayerInput>();
+                PlayerInput pi = tile.GetComponent<PlayerInput>();
                 pi.camera = Camera.main;
-                if(newFlags != 0x00)
-                {
-                    game.SetActive(false);
-                }
-                objects.Add(game);
-                position.x += 1;
+                position.x += TILE_STEP;
             }
-            position.z += 1;
+            position.y += TILE_STEP;
             position.x = 0;
         }
-        
+
         // Set the camera so that the whole terrain is in view. START
-        Vector3 top_left_corner    = new Vector3(-TILE_STEP * 0.5f, -TILE_STEP * 0.5f, 0);
+        Vector3 top_left_corner = new Vector3(-TILE_STEP * 0.5f, -TILE_STEP * 0.5f, 0);
         Vector3 bottom_left_corner = new Vector3(top_left_corner.x + (float)width * TILE_STEP, top_left_corner.y + (float)height * TILE_STEP, 0);
-        
+
         Transform camera_transform = Camera.main.GetComponent<Transform>();
         camera_transform.position = new Vector3(
             top_left_corner.x + (bottom_left_corner.x - top_left_corner.x) * 0.5f,
@@ -68,20 +63,5 @@ public class Our_Terrain : MonoBehaviour
     void Update()
     {
 
-    }
-
-    public int GetIndex(int uid)
-    {
-        int index = 0;
-        foreach (GameObject obj in objects)
-        {
-            if (obj.GetInstanceID() == uid)
-            {
-                return index;
-            }
-            index++;
-        }
-        Assert.AreEqual(1, 0);
-        return -1;
     }
 }
