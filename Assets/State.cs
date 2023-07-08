038456f15b02acce5830fe4230040da62fc126db
Delicious Lines <miketearon@mailfence.com>
2023-07-08 17:59:11 +0200

    Fixed a bot bug. Made a better isometric view.

--- Assets/State.cs
+++ Assets/State.cs
@@ -2,15 +2,17 @@
 public class State
 {
     // RAZ
-    public static int EMPTY = 0;
+    public const int EMPTY = 0;
     // new tile
-    public static int SPAWN_WATER = 1;
-    public static int SPAWN_SAND  = 2;
-    public static int SPAWN_LAND  = 3;
-    public static int SPAWN_TOWN  = 4;
+    public const int SPAWN_WATER = 1;
+    public const int SPAWN_SAND  = 2;
+    public const int SPAWN_LAND  = 3;
+    public const int SPAWN_TOWN  = 4;
 
     // action
-    public static int SPAWN_TEMPEST  = 5;
+    public const int SPAWN_TEMPEST  = 5;
+    
+    public static bool do_not_raytrace_this_frame = false;
 
     public static Tile originTile;
     private State()
--- Assets/Terrain/script/our_terrain.cs
+++ Assets/Terrain/script/our_terrain.cs
@@ -23,10 +23,13 @@ public class Our_Terrain : MonoBehaviour
     public static float closest_camera_zoom  = 2.0f;
     
     Vector3 camera_look_at_direction;
+    Vector3 map_centre;
+    float   camera_rotation_circle_radius = 100.0f;
+    float   camera_rotation_angle_degrees = 45.0f - 90.0f;
 
     static public List<Tile> tiles = new List<Tile>();
     
-    // Start is called before the first frame update
+    
     void Start()
     {
         Vector3 position = new Vector3(0, 0, 0);
@@ -88,22 +91,20 @@ public class Our_Terrain : MonoBehaviour
         Vector3 bottom_left_corner = new Vector3(top_left_corner.x + (float)width * TILE_STEP, 0, top_left_corner.z + (float)height * TILE_STEP);
         
         Transform camera_transform = Camera.main.GetComponent<Transform>();
-        camera_transform.position = new Vector3(
-            bottom_left_corner.x,
-            5,
-            bottom_left_corner.z
-        );
         
-        Vector3 where_to_look_at = new Vector3(
+        map_centre = new Vector3(
             top_left_corner.x + (bottom_left_corner.x - top_left_corner.x) * 0.5f,
             0,
             top_left_corner.z + (bottom_left_corner.z - top_left_corner.z) * 0.5f
         );
         
-        Vector3 direction_to_look_at = where_to_look_at - camera_transform.position;
-        camera_look_at_direction = direction_to_look_at.normalized;
+        float theta = Mathf.Deg2Rad * (camera_rotation_angle_degrees);
+        Vector3 camera_position_on_circle = new Vector3(Mathf.Cos(theta), 0, Mathf.Sin(theta)) * camera_rotation_circle_radius;
         
-        camera_transform.position = camera_transform.position - camera_look_at_direction * 100.0f;
+        camera_transform.position = map_centre + camera_position_on_circle + new Vector3(0, 100, 0);
+        
+        Vector3 direction_to_look_at = map_centre - camera_transform.position;
+        camera_look_at_direction = direction_to_look_at.normalized;
         
         Quaternion orientation = Quaternion.LookRotation(camera_look_at_direction, new Vector3(0, 1, 0));
         
@@ -129,9 +130,6 @@ public class Our_Terrain : MonoBehaviour
         
         // @ Zoom around the mouse position.
         // Handle camera zoom. END
-        
-        
-        // @ Handle camera rotation.
         
         
         // Handle camera panning. START
