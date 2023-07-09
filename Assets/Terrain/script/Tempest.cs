ca689ca1503d2b5bc9c885991ab94afe353994b3
Delicious Lines <miketearon@mailfence.com>
2023-07-09 16:17:57 +0200

    Fixed the storm does not disappear after reaching its target.

--- Assets/Terrain/script/Tempest.cs
+++ Assets/Terrain/script/Tempest.cs
@@ -10,7 +10,7 @@ public class Tempest : MonoBehaviour
     public Vector3 target;
     [SerializeField]
     private float speed = 0.8f;
-    public GameObject row;
+    //public GameObject row;
     void Start()
     {
         AudioSource audio_source = GetComponent<AudioSource>();
@@ -23,8 +23,12 @@ public class Tempest : MonoBehaviour
         
         t.position = Vector3.MoveTowards(t.position, target, speed * Time.deltaTime);
         // Check if the position of the cube and sphere are approximately equal.
-        Instantiate(row);
-        if (Vector3.Distance(t.position, target) < 0.001f)
+        //Instantiate(row);
+        
+        Vector2 storm_position_2d  = new Vector2(t.position.x, t.position.z);
+        Vector2 target_position_2d = new Vector2(target.x, target.z);
+        
+        if (Vector2.Distance(storm_position_2d, target_position_2d) < 0.001f)
         {
             there_is_an_active_tornado = false;
             
